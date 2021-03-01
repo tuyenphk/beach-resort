c31aafa14554065a3b124e192cec87b14385b70d
Tuyen Pham <tuh39709@temple.edu>
2021-02-28 20:41:09 -0500

    add banner

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+
 const Error = () => {
     return (
-        <div>
-            Hello from Error page
-        </div>
+        <Hero>
+            <Banner title="404" subtitle="Page not found">
+                <Link to="/" className="btn-primary">Return Home</Link>
+            </Banner>
+        </Hero>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
+
 const Home = () => {
     return (
-       <Hero />
+       <Hero>
+           <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms starting at $499">
+               <Link to="/room" className="btn-primary">Our Rooms</Link>
+           </Banner>
+       </Hero>
     )
 }
 
