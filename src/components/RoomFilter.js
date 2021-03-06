caee2e57b2eeac709e50fffa8efaf8985523aa45
Tuyen Pham <tuh39709@temple.edu>
2021-03-05 21:51:54 -0500

    filter room by capacity

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -18,11 +18,15 @@ export default function RoomFilter({rooms}) {
     types = types.map((item, index) =>{
         return <option value={item} key={index}>{item}</option>
     })
-
+    let people = getUnique(rooms, 'capacity');
+    people = people.map((item,index) => {
+        return <option key={index} value={item}>{item}</option>
+    })
     return (
         <section className="filter-container">
             <Title title="Search rooms" />
             <form className="filter-form">
+                {/* select type room */}
                 <div className="form-group">
                     <label htmlFor="type">Room Type</label>
                     <select name="type" 
@@ -31,6 +35,17 @@ export default function RoomFilter({rooms}) {
                             className="form-control"
                             onChange={handleChange}>
                         {types}
+                    </select>
+                </div>
+                {/* select guest type */}
+                <div className="form-group">
+                    <label htmlFor="capacity">Guests</label>
+                    <select name="capacity" 
+                            id="capacity" 
+                            value={capacity} 
+                            className="form-control"
+                            onChange={handleChange}>
+                        {people}
                     </select>
                 </div>
             </form>
--- src/context.js
+++ src/context.js
@@ -63,9 +63,17 @@ class RoomProvider extends Component {
 
     filterRooms = () =>{
         let {rooms, type, capacity, price, minSize, maxSize, breakfast, pets} = this.state;
+        // all the room
         let tempRooms = [...rooms];
+        // transform value
+        capacity = parseInt(capacity);
+        // filter by type room
         if (type !== 'all'){
             tempRooms = tempRooms.filter(room => room.type === type)
+        }
+        // filter by capacity
+        if (capacity !== 1){
+            tempRooms = tempRooms.filter(room => room.capacity >= capacity)
         }
         this.setState({
             sortedRooms: tempRooms
