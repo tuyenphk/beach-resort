028bf79703fdf031c55d5c3abc414d476f4fa030
Tuyen Pham <tuh39709@temple.edu>
2021-03-06 14:45:48 -0500

    filter for breakfast and pets

--- src/context.js
+++ src/context.js
@@ -54,7 +54,7 @@ class RoomProvider extends Component {
 
     handleChange = event =>{
         const target = event.target
-        const value = event.type === 'checkbox' ? target.checked:target.value
+        const value = target.type === 'checkbox' ? target.checked:target.value
         const name = event.target.name
         this.setState({
             [name]: value
@@ -78,7 +78,16 @@ class RoomProvider extends Component {
         }
         // filter by price
         tempRooms = tempRooms.filter(room => room.price <= price)
+        // filter by room size
         tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize)
+        // filter by breakfast
+        if (breakfast){
+            tempRooms = tempRooms.filter(room => room.breakfast === true)
+        }
+        // filter by pets
+        if (pets){
+            tempRooms = tempRooms.filter(room => room.pets === true)
+        }
         // change state
         this.setState({
             sortedRooms: tempRooms
